d98eb65823d11d2e8369f7c853c3560c76473726
Bruna <brunadias_30@hotmail.com>
2023-02-02 19:11:33 -0300

    adicionando relogio

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Botao from '../Botao'
+import Relogio from './Relogio'
 
 export default function Cronometro() {
     return (
         <div>
             <p>Escolha um card e inicie o cronômetro</p>
-
+            
+            <div>
+                <Relogio />
+            </div>
+            
             <Botao texto='Começar' />
        
         </div>
