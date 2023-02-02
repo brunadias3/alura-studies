badfa261b47bb077c05fe416c447bd07524148e3
Bruna <brunadias_30@hotmail.com>
2023-02-02 17:07:27 -0300

    Cronometro

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Botao from '../Botao'
+
+export default function Cronometro() {
+    return (
+        <div>
+            <p>Escolha um card e inicie o cronômetro</p>
+
+            <Botao texto='Começar' />
+       
+        </div>
+    )
+}
