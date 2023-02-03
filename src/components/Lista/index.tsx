927214b74f7e7c6f99cc0ad724ebfaa5a1648b82
Bruna <brunadias_30@hotmail.com>
2023-02-03 18:36:49 -0300

    tarefas de Itarefas

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
+import { ITarefa } from '../../types/tarefa'
 import style from './Lista.module.scss'
 import Item from './Item'
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    },{
-        tarefa:'JavaScript',
-        tempo: '01:00:00'
-    },{
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }]
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
     return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => 
-                <Item key={index} {...item} />
+                {tarefas.map((item, index) =>
+                    <Item key={index} {...item} />
                 )}
             </ul>
         </aside>
