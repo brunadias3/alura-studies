4c2af1346507c27f09ce42b8a13a223d730e495d
Bruna <brunadias_30@hotmail.com>
2023-02-06 18:36:36 -0300

    refatorando botao para funcao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
+interface Props {
   type?: "button" | "submit" | "reset" | undefined
-  texto: string
+  texto?: string
   onClick?: () => void
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.texto}
-      </button>
-    )
-  }
 }
+function Botao({ type, texto, onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+    >
+      {texto}
+    </button>
+  )
+}
+
+// class Botao1 extends React.Component<{
+//   type?: "button" | "submit" | "reset" | undefined
+//   texto: string
+//   onClick?: () => void
+// }> {
+//   render() {
+//     const { type = "button", onClick } = this.props;
+//     return (
+//       <button onClick={onClick} type={type} className={style.botao}>
+//         {this.props.texto}
+//       </button>
+//     )
+//   }
+// }
 
 export default Botao;
